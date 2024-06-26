a1eb24f85079f4a6afc7f85eb7d847a7e50c8b01
YashK76 <yashk6690@gmail.com>
2024-06-26 17:01:09 +0530

    Add files via upload

---
+++ voting_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int  age=18;
+	char nationality[]="Indian";
+	
+	if(age>=18){
+		if(strcmp(nationality,"Indian")==0){
+		printf("You are eliglible for voting");
+	}
+	}
+}
